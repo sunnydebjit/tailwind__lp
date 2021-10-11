08cc112283e73aaa309520e9113168f77409e43b
sunnydebjit <sunnydebjit@gmail.com>
2021-10-11 13:31:19 +0530

    labelling production build and deploying to vercel

--- components/placeholder.js
+++ components/placeholder.js
@@ -5,7 +5,7 @@ export default function Placeholder() {
 		<>
 			<main className='bg-green-900 min-h-screen flex flex-col justify-around items-center'>
 				<Framer__example>
-					<div className='bg-green-700 text-green-300 text-lg md:text-2xl mx-5 p-5 shadow-lg hover:shadow-2xl md:text-center rounded-lg border-b-8 border-green-900 hover:border-green-800 hover:bg-green-600 hover:text-green-200 hover:scale-110 transform transition duration-500'>
+					<div className='relative bg-green-700 text-green-50 text-lg md:text-2xl mx-5 p-5 shadow-lg hover:shadow-2xl md:text-center rounded-lg border-b-8 border-green-900 hover:border-green-800 hover:bg-green-600 hover:text-green-200 hover:scale-110 transform transition duration-500'>
 						<h1 className='font-bold'>Tailwind Next Js App</h1>
 						<p className='text-base md:text-lg'>
 							Initial starting point for this project. Feel free to edit me.
@@ -14,6 +14,9 @@ export default function Placeholder() {
 							Made with <span className=' inline-block animate-bounce'>💝</span>{' '}
 							by <span className='font-bold'>Debjit Majumdar</span>
 						</p>
+						<div className='absolute top-0 right-0 transform translate-x-3 -translate-y-3 text-xs md:text-sm font-bold text-red-100 bg-red-600 shadow-xl p-2 rounded'>
+							Production Build
+						</div>
 					</div>
 				</Framer__example>
 			</main>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+
 import Placeholder from '../components/placeholder';
 
 export default function Home() {
@@ -7,6 +8,10 @@ export default function Home() {
 			<Head>
 				<title>Tailwind Landing Page</title>
 				<meta name='description' content='copyright Debjit Majumdar' />
+				<meta http-equiv='Content-Type' content='text/html;charset=UTF-8' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<meta name='keywords' content='tailwind landing page' />
+				<meta http-equiv='X-UA-Compatible' content='IE=7' />
 				<link rel='icon' href='/dm-logo.svg' />
 			</Head>
 			<Placeholder />
